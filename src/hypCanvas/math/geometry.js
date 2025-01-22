1698180350fa9a8f0f9c8cc46845c84d66df9c16
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-22 11:26:24 -0600

    First commit: These helper functions are used in some of the shape components

---
+++ src/hypCanvas/math/geometry.js
@@ -0,0 +1,106 @@
+import { EPSILON, VERTICAL_AXIS_HEIGHT } from "../../constants";
+// import { getCanvasCoordinatesOld, getMathCoordinatesOld } from "./coordinates";
+
+export function getGeodesicParams(anchor1, anchor2, getMathCoordinates) {
+  const xSeparation = anchor2.canvasX - anchor1.canvasX;
+  const isACircle = Math.abs(xSeparation) > EPSILON;
+
+  const center = isACircle ? computeGeodesicCenter(anchor1, anchor2) : Infinity;
+  const radius = isACircle ? computeGeodesicRadius(anchor1, center, getMathCoordinates) : Infinity;
+
+  return { isACircle, center, radius }
+}
+
+export function getHypCircleParams(center, anchor, getCanvasCoordinates) {
+  const computeRadius = (eucCenter) => {
+    return Math.sqrt((eucCenter.canvasX - anchor.canvasX) ** 2 + (eucCenter.canvasY - anchor.canvasY) ** 2);
+  }
+
+  const deltaX = anchor.mathX - center.mathX;
+  if (Math.abs(deltaX) < EPSILON) {
+    const mathY = (anchor.mathY ** 2 + center.mathY ** 2) / (2 * anchor.mathY);
+    // const eucCenter = getCanvasCoordinatesOld(center.mathX, mathY);
+    const eucCenter = getCanvasCoordinates(center.mathX, mathY);
+    const radius = computeRadius(eucCenter);
+
+    return { eucCenter, radius };
+  }
+
+  const deltaY = anchor.mathY - center.mathY;
+  const midpointX = (anchor.mathX + center.mathX) / 2;
+  const midpointY = (anchor.mathY + center.mathY) / 2;
+  const slope = deltaY / deltaX;
+  const centerOfGeod = midpointX + midpointY * slope;
+
+  const mathY = anchor.mathY + (centerOfGeod - anchor.mathX) * (center.mathX - anchor.mathX) / anchor.mathY;
+  // const eucCenter = getCanvasCoordinatesOld(center.mathX, mathY);
+  const eucCenter = getCanvasCoordinates(center.mathX, mathY);
+  const radius = computeRadius(eucCenter);
+  
+  return { eucCenter, radius };
+}
+
+export function getSegmentParams(anchor1, anchor2, getMathCoordinates) {
+  const { isACircle, center, radius } = getGeodesicParams(anchor1, anchor2, getMathCoordinates);
+
+  const xSeparation = anchor2.canvasX - anchor1.canvasX;
+  const rightAnchor = xSeparation < 0 ? anchor1 : anchor2;
+  const leftAnchor = xSeparation < 0 ? anchor2 : anchor1;
+  const rotationAngle = computeAnchorAngle(rightAnchor, center, getMathCoordinates);
+  const arcAngle = computeAnchorAngle(leftAnchor, center, getMathCoordinates) - rotationAngle;
+
+  return { isACircle, center, radius, rotationAngle, arcAngle, anchor1, anchor2 };
+}
+
+export function getPolygonParams(anchors, getMathCoordinates) {
+  return anchors.map((anchor, idx) => {
+    const nextAnchor = anchors[(idx + 1) % anchors.length];
+    return getSegmentParams(anchor, nextAnchor, getMathCoordinates);
+  });
+}
+
+function computeGeodesicCenter(anchor1, anchor2) {
+  const deltaY = anchor2.mathY - anchor1.mathY;
+  const midpointX = (anchor2.mathX + anchor1.mathX) / 2;
+
+  if (Math.abs(deltaY) < EPSILON) {
+    return midpointX + window.innerWidth / 2;
+  }
+
+  const deltaX = anchor2.mathX - anchor1.mathX;
+  const midpointY = (anchor2.mathY + anchor1.mathY) / 2;
+  const slope = deltaY / deltaX;
+  const perpSlope = -1 / slope;
+  const yIntercept = midpointY - perpSlope * midpointX;
+  const mathCenter = -yIntercept / perpSlope;
+  
+  return mathCenter + window.innerWidth / 2;
+}
+
+function computeGeodesicRadius(anchor, center, getMathCoordinates) {
+  // const { mathX } = getMathCoordinatesOld(center, VERTICAL_AXIS_HEIGHT);
+  const { mathX } = getMathCoordinates(center, VERTICAL_AXIS_HEIGHT);
+  return Math.sqrt((anchor.mathX - mathX) ** 2 + (anchor.mathY) ** 2);
+}
+
+function computeAnchorAngle(anchor, center, getMathCoordinates) {
+  // const { mathX, mathY } = getMathCoordinatesOld(center, VERTICAL_AXIS_HEIGHT);
+  const { mathX, mathY } = getMathCoordinates(center, VERTICAL_AXIS_HEIGHT);
+  const angle = Math.atan2(anchor.mathY - mathY, anchor.mathX - mathX);
+  return (angle * 180) / Math.PI;
+}
+
+// function getSegmentInfo(coords, nextCoords) {
+//   const xSeparation = nextCoords.canvasX - coords.canvasX;
+//   const isACircle = Math.abs(xSeparation) > EPSILON;
+  
+//   const center = isACircle ? computeGeodesicCenter(nextCoords, coords) : Infinity;
+//   const radius = isACircle ? computeGeodesicRadius(nextCoords, center) : Infinity;
+
+//   const rightAnchor = xSeparation > 0 ? nextCoords : coords;
+//   const leftAnchor = xSeparation > 0 ? coords : nextCoords;
+//   const rotationAngle = computeAnchorAngle(rightAnchor, center);
+//   const arcAngle = computeAnchorAngle(leftAnchor, center) - rotationAngle;
+
+//   return { isACircle, center, radius, arcAngle, rotationAngle, coords, nextCoords };
+// }
