543c0cb919e4568ef24aed3a35da42cbb18cae0b
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-22 11:23:46 -0600

    Lifted state and handlers up to the app component

--- src/hypCanvas/HypCanvas.jsx
+++ src/hypCanvas/HypCanvas.jsx
@@ -1,84 +1,81 @@
-import { useState } from "react";
-import { Label, Layer, Stage, Tag, Text } from "react-konva";
+import { useEffect, useState, useRef } from "react";
+import { Label, Layer, Stage, Tag, Text, Rect, Group } from "react-konva";
+import Konva from "konva";
 
 import Axes from "./shapes/Axes";
 import Geodesic from "./shapes/Geodesic";
 import Point from "./shapes/Point";
-import { CLICKED_POINT_COLOR, CURSOR_COORD_COLOR, CURSOR_FONT_SIZE, VERTICAL_AXIS_HEIGHT } from "./constants";
-import { getPointerCoordinates } from "./coordinates";
+import { ACTIVE_POINT_COLOR, CURSOR_COORD_COLOR, CURSOR_FONT_SIZE, HOLD_DURATION_THRESHOLD, VERTICAL_AXIS_HEIGHT } from "../constants";
+// import { getMathCoordinatesOld, getMouseCoordinatesOld } from "./math/coordinates";
+import Horocycle from "./shapes/Horocycle";
+import { generateId } from "../util/generateId";
+import HypCircle from "./shapes/HypCircle";
+import Segment from "./shapes/Segment";
+import Polygon from "./shapes/Polygon";
+import CenterOfRotation from "./animation shapes/CenterOfRotation";
 
-export default function HypCanvas({ toolbarState, shapes, addShapes }) {
-  // const [shapes, setShapes] = useState([]);
-  const [activePoints, setActivePoints] = useState([]);
-
-  function handleOnClick(event) {
-    const { canvasX, canvasY, mathX, mathY } = getPointerCoordinates(event);
-
-    switch(toolbarState.clickTool) {
-      case 'point':
-        return addPoint(canvasX, canvasY);
-      case 'line':
-        return addGeodesic(canvasX, canvasY, mathX, mathY);
-    }
-
-    function addPoint(canvasX, canvasY) {
-      addShapes(<Point clickedX={canvasX} clickedY={canvasY} />)
-      // setShapes([ ...shapes, <Point clickedX={canvasX} clickedY={canvasY} /> ]);
-    }
-
-    function addGeodesic(canvasX, canvasY, mathX, mathY) {
-      if (!activePoints.length) {
-        setActivePoints([{ canvasX, canvasY, mathX, mathY }]);
-        return;
-      }
-
-      const firstClicked = [...activePoints].pop();
-      addShapes(<Geodesic clicked1={firstClicked} clicked2={{ canvasX, canvasY, mathX, mathY }} />);
-      // setShapes([ ...shapes, <Geodesic clicked1={firstClicked} clicked2={{ canvasX, canvasY, mathX, mathY }} />])
-      setActivePoints([])
-    }
-  }
-
-  const [cursorCoord, setCursorCoord] = useState(null);
-  function handleMouseMove(event) {
-    const coordinates = getPointerCoordinates(event);
-    if (coordinates.mathY < 0) {
-      setCursorCoord(null);
-      return;
-    }
-
-    setCursorCoord({ ...coordinates });
-  }
-
+export default function HypCanvas({
+  toolbarState,
+  mouseCoords,
+  onMouseDown,
+  onMouseMove,
+  onMouseUp,
+  onMouseLeave,
+  // canvasIsDragging,
+  originX,
+  getMathCoordinates,
+  zoomScale,
+  children
+}) {
   return (
     <Stage
       width={window.innerWidth}
       height={window.innerHeight}
-      onClick={handleOnClick}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={() => setCursorCoord(null)}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseLeave}
     >
+      {/* AXES LAYER */}
       <Layer>
-        <Axes />
-        {toolbarState.showCursorCoord && cursorCoord && 
-          <Text
-            x={cursorCoord.canvasX}
-            y={cursorCoord.canvasY}
-            text={`(${cursorCoord.mathX}, ${cursorCoord.mathY})`}
-            fill={CURSOR_COORD_COLOR}
-            fontSize={CURSOR_FONT_SIZE}
-            offsetY={CURSOR_FONT_SIZE * 1.1}
-            offsetX={-5}
-          />
+        <Axes 
+          originX={originX}
+          getMathCoordinates={getMathCoordinates}
+          toolbarState={toolbarState}
+          zoomScale={zoomScale}
+        />
+      </Layer>
+
+      {/* CURSOR LAYER */}
+      <Layer>
+        {/* {
+          toolbarState.showMouseCoords && mouseCoords && !canvasIsDragging && 
+            <Text
+              x={mouseCoords.canvasX}
+              y={mouseCoords.canvasY}
+              offsetX={-5}
+              offsetY={CURSOR_FONT_SIZE * 1.1}
+              text={`(${mouseCoords.mathX}, ${mouseCoords.mathY})`}
+              fill={CURSOR_COORD_COLOR}
+              fontSize={CURSOR_FONT_SIZE}
+            />
+        } */}
+        {
+          toolbarState.showMouseCoords && mouseCoords && 
+            <Text
+              x={mouseCoords.canvasX}
+              y={mouseCoords.canvasY}
+              offsetX={-5}
+              offsetY={CURSOR_FONT_SIZE * 1.1}
+              text={`(${mouseCoords.mathX}, ${mouseCoords.mathY})`}
+              fill={CURSOR_COORD_COLOR}
+              fontSize={CURSOR_FONT_SIZE}
+            />
         }
       </Layer>
 
-      <Layer>
-        {activePoints.map(({ canvasX, canvasY }, idx) => (
-          <Point key={idx} clickedX={canvasX} clickedY={canvasY} color={CLICKED_POINT_COLOR} />
-        ))}
-        {...shapes}
-      </Layer>
+      {/* SHAPES LAYER */}
+      <Layer>{ children }</Layer>
     </Stage>
   );
 }
