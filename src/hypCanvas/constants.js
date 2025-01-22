87264fd4684135d67b19f103a056673993d876d3
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-22 11:22:33 -0600

    Moved this file up the file-tree

--- src/hypCanvas/constants.js
+++
@@ -1,9 +0,0 @@
-export const VERTICAL_AXIS_HEIGHT = window.innerHeight * 0.95
-export const POINT_RADIUS = 5;
-export const POINT_COLOR = 'white';
-export const CLICKED_POINT_COLOR = "yellow";
-export const EPSILON = 1e-3;
-export const GEODESIC_COLOR = "white";
-export const DRAGGING_POINT_COLOR = "green";
-export const CURSOR_COORD_COLOR = "white";
-export const CURSOR_FONT_SIZE = 24;
