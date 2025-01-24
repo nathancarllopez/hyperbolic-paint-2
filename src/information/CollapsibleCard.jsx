be8be4cda0db6fedd4aa5173e95ef7aae3b59c8e
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-24 14:55:22 -0600

    First commit: Used in the Information component to display each section of text

---
+++ src/information/CollapsibleCard.jsx
@@ -0,0 +1,52 @@
+import './CollapsibleCard.css';
+import upArrow from "../assets/chevron-double-up.svg"
+import downArrow from "../assets/chevron-double-down.svg"
+
+import { useState } from "react";
+import Card from "react-bootstrap/Card"
+import Col from "react-bootstrap/Col"
+import Collapse from "react-bootstrap/Collapse"
+import Container from "react-bootstrap/Container"
+import Image from "react-bootstrap/Image"
+import Row from "react-bootstrap/Row"
+
+export default function CollapsibleCard({ title, startsOpen = false, children }) {
+  const [isIn, setIsIn] = useState(startsOpen);
+
+  return (
+    <Container className='border rounded'>
+      <Row className='align-items-center gx-0'>
+        <Col>
+          <h5 className="mb-0">{ title }</h5>
+        </Col>
+        <Col className="col-auto">
+          <button
+            id='collapseToggle'
+            className="btn"
+            onClick={ () => setIsIn(!isIn) }
+            aria-controls="collapse-container"
+            aria-expanded={ isIn }
+          >
+            <Image src={ isIn ? downArrow : upArrow } fluid/>
+          </button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Collapse in={isIn}>
+            {/* This extra div helps Bootstrap React make the collapse animation smooth */}
+            <div id="collapse-container">
+              <hr className="my-2"/>
+              <Card className='border-0'>
+                <Card.Body className='p-1'>
+                  { children }
+                </Card.Body>
+              </Card>
+            </div>
+          </Collapse>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
