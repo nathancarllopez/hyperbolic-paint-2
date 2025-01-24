bbe5a89f443af20cf822696b07663091b937847f
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-24 14:58:50 -0600

    Moved these files from src into src/util

---
+++ src/util/constants.js
@@ -0,0 +1,45 @@
+export const VERTICAL_AXIS_HEIGHT = window.innerHeight * 0.95
+export const POINT_RADIUS = 5;
+export const POINT_COLOR = 'white';
+export const ACTIVE_POINT_COLOR = "yellow";
+export const EPSILON = 1e-3;
+export const GEODESIC_COLOR = "white";
+export const DRAGGING_POINT_COLOR = "green";
+export const CURSOR_COORD_COLOR = "white";
+export const CURSOR_FONT_SIZE = 24;
+export const FOCUSED_POINT_COLOR = "purple";
+export const POINT_SHADOW_COLOR = "purple";
+export const HOROCYCLE_COLOR = "white";
+export const HOROCYCLE_CENTER_COLOR = "red";
+export const HOROCYCLE_ANCHOR_COLOR = "blue";
+export const SELECTED_SHAPE_COLOR = "purple";
+export const FIXED_ANCHOR_COLOR = "red";
+export const FREE_ANCHOR_COLOR = "blue";
+export const CIRCLE_COLOR = "white";
+export const CIRCLE_CENTER_COLOR = "red";
+export const CIRCLE_ANCHOR_COLOR = "blue";
+export const POLYGON_COLOR = "white";
+export const SEGMENT_COLOR = "white";
+export const HOLD_DURATION_THRESHOLD = 500;
+export const CENTER_ROTATION_COLOR = "orange";
+export const DASH_LENGTH = 10;
+export const DASH_SEPARATION = 5;
+export const INITIAL_TOOLBARSTATE = {
+  // clickTool: 'horocycle',
+  // clickTool: 'point',
+  clickTool: 'geodesic',
+  // clickTool: 'circle',
+  // clickTool: 'segment',
+  // clickTool: 'polygon',
+  // clickTool: 'rotation',
+  showMouseCoords: true,
+  showHistoryControls: true,
+  showAxisTicks: true
+};
+export const ANIMATION_TOOLNAMES = ['rotation'];
+export const AXIS_TICK_LENGTH = 4;
+export const AXIS_TICK_SEPARATION = 50;
+export const AXES_COLOR = "gray";
+export const AXES_LABEL_FONT_SIZE = 12;
+export const INITIAL_ORIGIN_X = (window.innerWidth / 2) * 1;
+export const AXES_STROKE_WIDTH = 2;
