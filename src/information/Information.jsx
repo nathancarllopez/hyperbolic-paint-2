48c224892d3d86aec4803e6b9b7a9fc88f57fa8e
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-24 17:23:27 -0600

    Added a "learn" section so that the welcome text isn't so long. Finished the "instructions" section

--- src/information/Information.jsx
+++ src/information/Information.jsx
@@ -12,11 +12,43 @@ export default function Information() {
     <Stack gap={3}>
       <CollapsibleCard title={"Welcome to Hyperbolic Paint"} startsOpen={true}>
         <Card.Text>
-          Explore <Link hrefCode={'hypGeo'}>hyperbolic geometry</Link> in the <Link hrefCode='halfPlane'>Poincaré half-plane model!</Link> Open the toolbar with the button at the top right of the screen and start drawing, or read on to learn more about the underlying mathematics.
+          Explore <Link hrefCode={'hypGeo'}>hyperbolic geometry</Link> in the <Link hrefCode='halfPlane'>Poincaré half-plane model!</Link> If you like learning by doing, start drawing by opening the toolbar with the button at the top right of your screen. If you want more detailed instructions, check out the "Instructions" section below. Or, if you want to learn more about the underlying mathematics, read the "Learn" section.
+        </Card.Text>
+      </CollapsibleCard>
+
+      <CollapsibleCard title={"Instructions"}>
+        <Card.Text>
+          In general, all shapes are draggable, and changes made to shapes can be removed/recovered using the undo/redo buttons, respectively. Shapes can also be selected and deleted using the delete button. These buttons themselves can also be moved on the screen if you'd like to reposition out of the way while drawing. Moreover, the canvas itself can be dragged horizontally by clicking anywhere that a shape isn't already drawn.
         </Card.Text>
 
         <Card.Text>
-          Like standard geometry (so-called <Link hrefCode='eucGeo'>"Euclidean geometry"</Link>), the objects of study in hyperbolic geometry are shapes like points, lines, and polygons. The fundamental difference is that Euclidean geometry is done on a flat plane, while hyperbolic geometry studies shapes on a <Link hrefCode='negCurv'>negatively curved surface.</Link> 
+          Note: In the instructions below, "click" is short for "click or touch if you're on a mobile device". Moreover, clicking below the x-axis is not allowed (see the "Learn" section to understand why).
+        </Card.Text>
+
+        <Card.Text>
+          <strong>Point:</strong> Click once to create a point.
+        </Card.Text>
+
+        <Card.Text>
+          <strong>Line and Line Segment:</strong> Click twice to create a line.
+        </Card.Text>
+
+        <Card.Text>
+          <strong>Circle:</strong> Click twice to create a circle. Your first click determines the circle's center, and the second determines the radius.
+        </Card.Text>
+
+        <Card.Text>
+          <strong>Horocycle:</strong> Click once to create a horocycle. Your click will place the center of the horocycle, and the horocycle itself will be tangent to the x-axis directly below the center.
+        </Card.Text>
+
+        <Card.Text>
+          <strong>Polygon</strong>: Click as many times as you like to create a polygon. The resulting vertices will be connected with line segments in the order you clicked. To place your last point and finish the polygon, click and hold.
+        </Card.Text>
+      </CollapsibleCard>
+
+      <CollapsibleCard title={"Learn"}>
+      <Card.Text>
+          Like standard geometry (so-called <Link hrefCode='eucGeo'>"Euclidean geometry"</Link>), the objects of study in hyperbolic geometry are shapes like points, lines, and polygons. The fundamental difference is that Euclidean geometry is done on a flat plane, while hyperbolic geometry studies shapes on a <Link hrefCode='negCurv'>negatively curved surface.</Link> As can be seen in the figure below, the shortest path between any two points is no longer a straight line!
         </Card.Text>
 
         <Container className="text-center">
@@ -27,26 +59,9 @@ export default function Information() {
             </Figure.Caption>
           </Figure>
         </Container>
-      </CollapsibleCard>
 
-      <CollapsibleCard title={"Instructions"}>
         <Card.Text>
-          Point:
-        </Card.Text>
-        <Card.Text>
-          Line:
-        </Card.Text>
-        <Card.Text>
-          Circle:
-        </Card.Text>
-        <Card.Text>
-          Horocycle:
-        </Card.Text>
-        <Card.Text>
-          Line Segment:
-        </Card.Text>
-        <Card.Text>
-          Polygon:
+          Hyperbolic geometry is incredibly rich, both mathematically and <Link hrefCode="escher">aesthetically.</Link> 
         </Card.Text>
       </CollapsibleCard>
 
