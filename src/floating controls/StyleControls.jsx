f5c0880c37c4e26d96189430dc58dbfc8d747ed2
Nathan Lopez <nathancarllopez@gmail.com>
2025-01-24 14:51:35 -0600

    Moved from the toolbar folder to the floating controls folder

---
+++ src/floating controls/StyleControls.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import Draggable from "react-draggable";
+
+export default function StyleControls({
+  deleteDisabled,
+  onDeleteClick,
+}) {
+  const nodeRef = useRef(null);
+  const cardBodyRef = useRef(null);
+
+  useEffect(() => {
+    const cardBody = cardBodyRef.current;
+    const cardBodyStyles = getComputedStyle(cardBody);
+    const width = parseFloat(cardBodyStyles.getPropertyValue('width'));
+    
+    const card = nodeRef.current;
+    card.style.left = `${(window.innerWidth - width) / 2}px`;
+  }, []);
+
+  const buttonInfo = [
+    { key: 'delete', label: "Delete", disabled: deleteDisabled, onClick: onDeleteClick },
+  ];
+
+  return (
+    <Draggable nodeRef={nodeRef}>
+      <Card
+        ref={nodeRef}
+        style={{
+          position: "fixed",
+          top: 0,
+          // left: "10rem",
+          margin: "1rem",
+          zIndex: "10"
+        }}
+      >
+        <Card.Body ref={cardBodyRef}>
+          <Container>
+            <Row>
+              {
+                buttonInfo.map(({ key, label, disabled, onClick }) => (
+                  <Col key={key}>
+                    <Button
+                      disabled={disabled}
+                      onClick={onClick}
+                    >
+                      { label }
+                    </Button>
+                  </Col>
+                ))
+              }
+            </Row>
+          </Container>
+        </Card.Body>
+      </Card>
+    </Draggable>
+  );
+}
