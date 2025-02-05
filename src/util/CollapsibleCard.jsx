df3bad554d71357d118b415637ac905b13d9107c
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-05 09:48:07 -0600

    Actual first commit: HOC used in Suggestions.jsx

--- src/util/CollapsibleCard.jsx
+++ src/util/CollapsibleCard.jsx
@@ -2,7 +2,7 @@ import './CollapsibleCard.css';
 import upArrow from "../assets/chevron-double-up.svg"
 import downArrow from "../assets/chevron-double-down.svg"
 
-import { useRef, useState } from "react";
+// import { useRef, useState } from "react";
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Collapse from "react-bootstrap/Collapse"
@@ -54,60 +54,3 @@ export default function CollapsibleCard({
     </Container>
   );
 }
-
-// export default function CollapsibleCard({ title, startsOpen = false, children }) {
-//   const [isIn, setIsIn] = useState(startsOpen);
-
-//   return (
-//     <Container className='border rounded'>
-//       <Row className='align-items-center gx-0' onClick={() => setIsIn(!isIn)} style={{cursor: "pointer"}}>
-//         <Col>
-//           <h5 className="mb-0">{ title }</h5>
-//         </Col>
-//         <Col className="col-auto">
-//           <button
-//             id='collapseToggle'
-//             className="btn"
-//             // onClick={ () => setIsIn(!isIn) }
-//             aria-controls="collapse-container"
-//             aria-expanded={ isIn }
-//           >
-//             <Image src={ isIn ? downArrow : upArrow } fluid/>
-//           </button>
-//         </Col>
-//       </Row>
-//       {/* <Row className='align-items-center gx-0'>
-//         <Col>
-//           <h5 className="mb-0">{ title }</h5>
-//         </Col>
-//         <Col className="col-auto">
-//           <button
-//             id='collapseToggle'
-//             className="btn"
-//             onClick={ () => setIsIn(!isIn) }
-//             aria-controls="collapse-container"
-//             aria-expanded={ isIn }
-//           >
-//             <Image src={ isIn ? downArrow : upArrow } fluid/>
-//           </button>
-//         </Col>
-//       </Row> */}
-
-//       <Row>
-//         <Col>
-//           <Collapse in={isIn}>
-//             {/* This extra div helps Bootstrap React make the collapse animation smooth */}
-//             <div id="collapse-container">
-//               <hr className="my-2"/>
-//               <Card className='border-0'>
-//                 <Card.Body className='p-1'>
-//                   { children }
-//                 </Card.Body>
-//               </Card>
-//             </div>
-//           </Collapse>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// }
