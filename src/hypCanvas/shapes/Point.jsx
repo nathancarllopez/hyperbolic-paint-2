66ac8b2d24e004f5e6610ad033c7da0e808ef5d5
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-05 00:46:49 -0600

    Changed selected visual indicator from a color change to a bounding box. Still want to tweak the size of the box so it fits snugger

--- src/hypCanvas/shapes/Point.jsx
+++ src/hypCanvas/shapes/Point.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Circle, Group } from "react-konva";
-import { POINT_COLOR, POINT_RADIUS, FOCUSED_POINT_COLOR, SELECTED_SHAPE_COLOR, VERTICAL_AXIS_HEIGHT } from "../../util/constants";
+import { Circle, Group, Rect } from "react-konva";
+import { POINT_COLOR, POINT_RADIUS, FOCUSED_POINT_COLOR, SELECTED_SHAPE_COLOR, VERTICAL_AXIS_HEIGHT, SELECT_BOX_COLOR } from "../../util/constants";
 
 export default function Point({
   id = "",
@@ -8,38 +8,16 @@ export default function Point({
   clickedX,
   clickedY,
   getMathCoordinates,
-  color = POINT_COLOR,
+  color,
+  strokeWidth,
   isDraggable = true,
   onDragStart = () => {},
   onDragMove = () => {},
   onDragEnd = () => {},
   isSelected = false,
 }) {
+  // console.log(clickedX, clickedY)
   const [isFocused, setIsFocused] = useState(false);
-  const pointColor = isFocused ? FOCUSED_POINT_COLOR :
-                     isSelected ? SELECTED_SHAPE_COLOR :
-                     color;
-
-  const KonvaCircle = (shapeId, shapeName) => (
-    <Circle
-      id={shapeId}
-      name={shapeName}
-      x={clickedX}
-      y={clickedY}
-      radius={POINT_RADIUS}
-  
-      stroke={ pointColor }
-      fill={ pointColor }
-  
-      onMouseEnter={() => setIsFocused(true)}
-      onMouseLeave={() => setIsFocused(false)}
-      
-      draggable={isDraggable}
-      onDragStart={onDragStart}
-      onDragMove={handleDragMove}
-      onDragEnd={onDragEnd}
-    />
-  );
 
   function handleDragMove(event) {
     const konvaPoint = event.target;
@@ -55,18 +33,62 @@ export default function Point({
     }
 
     const newParams = getMathCoordinates(konvaPoint.x(), konvaPoint.y());
-    const recipeId = konvaPoint.id();
+    const recipeId = konvaPoint.getParent().id();
 
     onDragMove(event, newParams, recipeId);
   }
 
   return (
     <>
+      <Rect
+        x={clickedX - 1.5 * (POINT_RADIUS + strokeWidth)}
+        y={clickedY - 1.5 * (POINT_RADIUS + strokeWidth)}
+        width={3 * (POINT_RADIUS + strokeWidth)}
+        height={3 * (POINT_RADIUS + strokeWidth)}
+        stroke={SELECT_BOX_COLOR}
+        listening={false}
+        strokeWidth={isFocused || isSelected ? 1 : 0}
+      />
       {/* We do this so that all shapes (including those that have Points as subcomponents) can access their id via shape.getParent().id() */}
       {
-        id !== "" ?
-          <Group id={id}>{KonvaCircle(undefined, name)}</Group> :
-          KonvaCircle(id, name)
+        id !== "" ? 
+          <Group id={id}>
+            <Circle
+              name={name}
+              x={clickedX}
+              y={clickedY}
+              radius={POINT_RADIUS + strokeWidth}
+          
+              stroke={color}
+              fill={color}
+          
+              onMouseEnter={() => setIsFocused(true)}
+              onMouseLeave={() => setIsFocused(false)}
+              
+              draggable={isDraggable}
+              onDragStart={onDragStart}
+              onDragMove={handleDragMove}
+              onDragEnd={onDragEnd}
+            />
+          </Group> :
+          <Circle
+            id={id}
+            name={name}
+            x={clickedX}
+            y={clickedY}
+            radius={POINT_RADIUS + strokeWidth}
+        
+            stroke={color}
+            fill={color}
+        
+            onMouseEnter={() => setIsFocused(true)}
+            onMouseLeave={() => setIsFocused(false)}
+            
+            draggable={isDraggable}
+            onDragStart={onDragStart}
+            onDragMove={handleDragMove}
+            onDragEnd={onDragEnd}
+        />
       }
     </>
   );
