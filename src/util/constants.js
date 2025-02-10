69f2258d7248c5899d8358eb487505003d3056e6
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-10 10:53:36 -0600

    Clean up

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,46 +1,50 @@
-export const VERTICAL_AXIS_HEIGHT = window.innerHeight * 0.95
-export const POINT_RADIUS = 5;
-export const POINT_COLOR = 'white';
+/** For mobile devices */
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+/** For canvas */
 export const ACTIVE_POINT_COLOR = "yellow";
-export const EPSILON = 1e-3;
-export const GEODESIC_COLOR = "white";
-export const DRAGGING_POINT_COLOR = "green";
 export const CURSOR_COORD_COLOR = "white";
 export const CURSOR_FONT_SIZE = 24;
-export const FOCUSED_POINT_COLOR = "purple";
-export const POINT_SHADOW_COLOR = "purple";
-export const HOROCYCLE_COLOR = "white";
-export const HOROCYCLE_CENTER_COLOR = "red";
-export const HOROCYCLE_ANCHOR_COLOR = "blue";
-export const SELECTED_SHAPE_COLOR = "purple";
 export const FIXED_ANCHOR_COLOR = "red";
 export const FREE_ANCHOR_COLOR = "blue";
-export const CIRCLE_COLOR = "white";
-export const CIRCLE_CENTER_COLOR = "red";
-export const CIRCLE_ANCHOR_COLOR = "blue";
-export const POLYGON_COLOR = "white";
-export const SEGMENT_COLOR = "white";
-export const HOLD_DURATION_THRESHOLD = 500;
-export const CENTER_ROTATION_COLOR = "orange";
-export const AXIS_TRANSLATION_COLOR = "orange";
+export const ANIMATION_SHAPE_COLOR = "orange";
 export const DASH_LENGTH = 10;
 export const DASH_SEPARATION = 20;
-export const INITIAL_SETTINGS = {
-  showMouseCoords: true,
-  showAxisTicks: true,
-  showToolbarInstructions: true,
-  pointRadius: 5,
-  // pointHitRadius: 5,
-}
-export const ANIMATION_TOOLNAMES = ['rotation', 'translation'];
-export const DRAWING_TOOLNAMES = ['point', 'horocycle', 'geodesic', 'circle', 'segment', 'polygon'];
 export const AXIS_TICK_LENGTH = 4;
 export const AXIS_TICK_SEPARATION = 50;
 export const AXES_COLOR = "gray";
 export const AXES_LABEL_FONT_SIZE = 12;
-export const INITIAL_ORIGIN_X = (window.innerWidth / 2) * 1;
 export const AXES_STROKE_WIDTH = 2;
-export const ERROR_MESSAGE_DURATION = 3 * 1000;
-export const ANIM_SPEED_DAMPENER = 0.1;
-export const TOAST_DELAY = 5000;
 export const SELECT_BOX_COLOR = "white";
+
+/** Math */
+export const EPSILON = 1e-3;
+
+/** Initial state values */
+export const INITIAL_SETTINGS = {
+  showMouseCoords: true,
+  showAxisTicks: true,
+  showToolbarInstructions: true,
+  pointRadius: 8,
+  holdDuration: 500,
+  toastDuration: 5000
+};
+export const INITIAL_ORIGIN_COORDS = {
+  x: window.innerWidth / 2,
+  y: isTouchDevice ? window.innerHeight * 0.90 : window.innerHeight * 0.95
+};
+export const INITIAL_CANVAS_DIMENSIONS = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
+/** For code */
+export const ANIMATION_TOOLNAMES = ['rotation', 'translation'];
+
+/** Candidates for settings */
+export const ERROR_MESSAGE_DURATION = 3 * 1000;
+
+/** To be deleted */
+export const ANIM_SPEED_DAMPENER = 0.1;
+
+
