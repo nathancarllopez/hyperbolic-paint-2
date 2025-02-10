9b653b258b9445b0cb48f1262770bb7641e51f1c
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-10 10:50:29 -0600

    Added a CollapsibleCard layer to make the controls smaller but not hidden

--- src/toolbar/AnimationControls.jsx
+++ src/toolbar/AnimationControls.jsx
@@ -1,26 +1,38 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+import InputGroup from "react-bootstrap/InputGroup";
 import Stack from "react-bootstrap/Stack";
-import FloatingDraggableCard from "../util/FloatingDraggableCard";
 import { ANIMATION_TOOLNAMES } from "../util/constants";
+import FloatingDraggableCard from "../util/FloatingDraggableCard";
+import CollapsibleCard from "../util/CollapsibleCard";
+import playIcon from "../assets/play-fill.svg";
+import pauseIcon from "../assets/pause-fill.svg";
 
 export default function AnimationControls({
   history,
   isAnimating, setIsAnimating,
   animationSpeed, setAnimationSpeed
 }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  const initialPlacement = isTouchDevice ?
+  { left: "1rem", bottom: "3rem" } :
+  { left: "1rem", bottom: "4rem" };
+
   const { snapshots, currIdx } = history;
-  const animationShape = snapshots[currIdx].find(recipe => ANIMATION_TOOLNAMES.includes(recipe.name));
+  const animationShape = snapshots[currIdx].find(drawing => ANIMATION_TOOLNAMES.includes(drawing.name));
   const animationName = animationShape?.name || null;
   const speedUnits = (() => {
     if (animationName === null) return "n/a";
 
     switch(animationName) {
       case 'rotation': {
-        return "Degrees / second";
+        return "deg/s";
       }
       case 'translation': {
-        return "Units / second";
+        return "units/s";
       }
       default: {
         throw new Error(`Unexpected animation name: ${animationName}`)
@@ -30,35 +42,40 @@ export default function AnimationControls({
 
   return (
     <FloatingDraggableCard
-      title="Animation Controls"
-      placement={{ left: 0, bottom: 0 }}
-      centered={false}
+      placement={initialPlacement}
+      cardBodyStyles={{ 
+        padding: 0,
+        width: isTouchDevice ? "145px" : 'auto'
+      }}
     >
-      <Stack direction="horizontal" gap={3}>
-        <Button
-          disabled={animationName === null}
-          onClick={() => setIsAnimating(prev => !prev)}
-          onTouchStart={() => setIsAnimating(prev => !prev)}
-        >
-          { isAnimating ? "Pause" : "Play" }
-        </Button>
+      <CollapsibleCard
+        title={"Controls"}
+        isOpen={isOpen}
+        toggleIsOpen={() => setIsOpen(!isOpen)}
+      >
+        <Stack gap={2}>
+          <Button
+            disabled={animationSpeed === "" || animationName === null}
+            onClick={() => setIsAnimating(!isAnimating)}
+            onTouchStart={() => setIsAnimating(!isAnimating)}
+          >
+            <Image src={ isAnimating ? pauseIcon : playIcon } fluid/>
+          </Button>
 
-        <Form>
-          <Form.Group controlId="animation-speed">
-            <Form.Label>
-              Speed: ({speedUnits})
-            </Form.Label>
+          <InputGroup className="mb-1">
             <Form.Control
               type="number"
+              id="animation-speed-input"
               disabled={animationName === null}
               onChange={event => setAnimationSpeed(event.target.value)}
               value={animationSpeed}
               min={-10}
               max={10}
             />
-          </Form.Group>
-        </Form>
-      </Stack>
+            <InputGroup.Text>{speedUnits}</InputGroup.Text>
+          </InputGroup>
+        </Stack>        
+      </CollapsibleCard>
     </FloatingDraggableCard>
   );
 }
