92369f0a1ca7cb388977b03fbbcfa0f4e12cd7bf
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-10 10:54:54 -0600

    Removed default title, and allowed more styles to be put into placement. Perhaps the prop should be called 'styles' now?

--- src/util/FloatingDraggableCard.jsx
+++ src/util/FloatingDraggableCard.jsx
@@ -3,10 +3,10 @@ import Card from "react-bootstrap/Card";
 import Draggable from "react-draggable";
 
 export default function FloatingDraggableCard({
-  title = "",
   handleDragStop = () => {},
+  cardBodyStyles = undefined,
   placement,
-  centered,
+  centered = false,
   children
 }) {
   const nodeRef = useRef(null);
@@ -24,7 +24,6 @@ export default function FloatingDraggableCard({
 
   const cardStyle = {
     position: "fixed",
-    margin: "1rem",
     zIndex: "10",
     ...placement
   };
@@ -32,21 +31,61 @@ export default function FloatingDraggableCard({
   return (
     <Draggable
       nodeRef={nodeRef} 
-      cancel="input" 
+      cancel="input"
       onStop={handleDragStop}
     >
       <Card ref={nodeRef} style={cardStyle}>
-        <Card.Body ref={cardBodyRef}>
-
-          {
-            title !== "" &&
-              <Card.Title><span className="me-auto">{ title }</span></Card.Title>
-          }
-
+        <Card.Body ref={cardBodyRef} style={cardBodyStyles}>
           { children }
-
         </Card.Body>
       </Card>
     </Draggable>
   );
 }
+
+// export default function FloatingDraggableCard({
+//   title = "",
+//   handleDragStop = () => {},
+//   placement,
+//   centered,
+//   children
+// }) {
+//   const nodeRef = useRef(null);
+//   const cardBodyRef = useRef(null);
+
+//   useEffect(() => {
+//     if (!centered) return;
+
+//     const cardBody = cardBodyRef.current;
+//     const cardBodyWidth = parseFloat(getComputedStyle(cardBody).getPropertyValue('width'));
+
+//     const card = nodeRef.current;
+//     card.style.left = `${(window.innerWidth - cardBodyWidth) / 2}px`;
+//   }, []);
+
+//   const cardStyle = {
+//     position: "fixed",
+//     margin: "1rem",
+//     zIndex: "10",
+//     ...placement
+//   };
+
+//   return (
+//     <Draggable
+//       nodeRef={nodeRef} 
+//       cancel="input" 
+//       onStop={handleDragStop}
+//     >
+//       <Card ref={nodeRef} style={cardStyle}>
+//         <Card.Body ref={cardBodyRef}>
+//           {
+//             title !== "" &&
+//               <Card.Title><span className="me-auto">{ title }</span></Card.Title>
+//           }
+
+//           { children }
+//         </Card.Body>
+//       </Card>
+//     </Draggable>
+//   );
+// }
