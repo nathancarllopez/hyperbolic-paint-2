84ee9ef4a100191db044b5773501163a9e196dd7
Nathan Lopez <nathancarllopez@gmail.com>
2025-02-10 10:38:14 -0600

    Added tool icon for non-expanded toolbar and mobile support with onTouchStart

--- src/fabDrawer/Fab.jsx
+++ src/fabDrawer/Fab.jsx
@@ -2,31 +2,35 @@ import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import infoIcon from "../assets/info.svg";
 import gearIcon from "../assets/gear.svg";
+import toolIcon from "../assets/tools.svg";
 
-export default function Fab({ placement, onClick, icon }) {
+export default function Fab({
+  placement,
+  onClick,
+  icon,
+  onTouchStart = () => {}
+}) {
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
   const allIconInfo = {
     "Information": { src: infoIcon, alt: "Information drawer toggle" },
-    "Settings": { src: gearIcon, alt: "Settings drawer toggle" }
+    "Settings": { src: gearIcon, alt: "Settings drawer toggle" },
+    "Toolbar": { src: toolIcon, alt: "Toolbar expand toggle" }
   };
   const iconInfo = allIconInfo[icon] || {};
 
   return (
     <div style={{
-      position: "fixed",
-      margin: "1rem",
       zIndex: "10",
       ...placement
     }}>
       <Button
         variant="primary"
         className="rounded-circle"
-        size="lg"
+        size={isTouchDevice ? "sm" : "lg"}
         onClick={onClick}
+        onTouchStart={onTouchStart}
       >
-        <Image
-          src={ iconInfo.src }
-          alt={ iconInfo.alt }
-        />
+        <Image src={ iconInfo.src } alt={ iconInfo.alt }/>
       </Button>
     </div>
   );
